fce54361560b03abb529060a1cb474dc324751bc
Matt Lawrence <matt.lawrence86@gmail.com>
2012-12-01 22:03:01 -0500

    Making the app do a test connection.

--- src/main/scala/com/z0rchain/App.scala
+++ src/main/scala/com/z0rchain/App.scala
@@ -1,5 +1,7 @@
 package com.z0rchain
 
+import org.jivesoftware.smack._
+
 /**
  * @author ${user.name}
  */
@@ -8,8 +10,15 @@ object App {
   def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
   
   def main(args : Array[String]) {
-    println( "Hello World!" )
-    println("concat arguments = " + foo(args))
+      val config = new ConnectionConfiguration("z0rchain.com");
+      config.setCompressionEnabled(true);
+      config.setSASLAuthenticationEnabled(true);
+
+      val connection = new XMPPConnection(config);
+      connection.connect();
+      connection.login("test", "test", "test");
+
+      connection.connect();
   }
 
 }
