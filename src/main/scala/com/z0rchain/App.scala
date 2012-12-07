9d59cfce8a5edf3e4ff63c1bdea520e7d15d0cd1
Matt Lawrence <matt.lawrence86@gmail.com>
2012-12-06 22:56:39 -0500

    He can now join a muc! Though he leaves quite quickly...

--- src/main/scala/com/z0rchain/App.scala
+++ src/main/scala/com/z0rchain/App.scala
@@ -1,24 +1,27 @@
 package com.z0rchain
 
-import org.jivesoftware.smack._
+import org.jivesoftware.smack.{ConnectionConfiguration, XMPPConnection}
+import org.jivesoftware.smackx.muc.MultiUserChat
 
 /**
  * @author ${user.name}
  */
 object App {
-  
+
   def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
-  
+
   def main(args : Array[String]) {
-      val config = new ConnectionConfiguration("z0rchain.com");
-      config.setCompressionEnabled(true);
-      config.setSASLAuthenticationEnabled(true);
+    val config = new ConnectionConfiguration("z0rchain.com");
+    config.setCompressionEnabled(true);
+    config.setSASLAuthenticationEnabled(true);
 
-      val connection = new XMPPConnection(config);
-      connection.connect();
-      connection.login("test", "test", "test");
+    val connection = new XMPPConnection(config);
+    connection.connect();
+    connection.login("test", "test", "test");
 
-      connection.connect();
+    val muc = new MultiUserChat(connection, "test@conference.z0rchain.com")
+
+    muc.join("testbot")
   }
 
 }
