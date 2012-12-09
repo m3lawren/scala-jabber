7771a4636bb3ba97b23e3e02f4d9119216b6eccd
Matt Lawrence <matt.lawrence86@gmail.com>
2012-12-08 21:45:44 -0500

    Adding a ListenHook.
    A basic hook that lets it listen to messages but not respond.

---
+++ jabber/src/main/scala/com/z0rchain/jabber/hook/ListenHook.scala
@@ -0,0 +1,17 @@
+package com.z0rchain.jabber.hook
+
+import org.jivesoftware.smack.packet._
+
+class ListenHook(handler: ListenHook.Handler) extends Hook {
+
+  def handleMessage(message: Message): Option[String] = {
+    handler(message.getBody(), message.getFrom())
+    None
+  }
+}
+ 
+object ListenHook {
+  type Handler = (/*message:*/ String, /*from:*/ String) => Unit
+
+  def apply(handler: Handler) = new ListenHook(handler)
+}
